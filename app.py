ec88e47743c7230870abc0e7594c1a90cacd859f
naeun-koo <59506897+naeun-koo@users.noreply.github.com>
2020-02-02 18:17:19 +0900

    4주차 과제

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
 
+from pymongo import MongoClient           # pymongo를 임포트 하기(패키지 인스톨 먼저 해야겠죠?)
+client = MongoClient('localhost', 27017)  # mongoDB는 27017 포트로 돌아갑니다.
+db = client.dbsparta                      # 'dbsparta'라는 이름의 db를 만듭니다.
+
 # 타겟 URL을 읽어서 HTML를 받아오고,
 headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
 data = requests.get('https://www.genie.co.kr/chart/top200?ditc=D&rtm=N&ymd=20190908',headers=headers)
@@ -12,6 +16,18 @@ soup = BeautifulSoup(data.text, 'html.parser')
 
 songs = soup.select('#body-content > div.newest-list > div > table > tbody > tr')
 
+#내용 크롤링
 rank = 1
-for song in songs
-    title = song.select_one
+for song in songs:
+    title = song.select_one('td.info > a.title').text.strip()
+    artist = song.select_one('td.info > a.artist').text
+    print(rank,title,artist)
+
+    doc = {
+        'rank': rank,
+        'title': title,
+        'artist': artist
+    }
+    db.songs.insert_one(doc)
+
+    rank += 1
